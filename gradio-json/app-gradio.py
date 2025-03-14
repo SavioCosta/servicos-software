c50dab3d315f66d196ffe70e1a96c66774c229e7
Savio Costa <saviao@gmail.com>
2025-03-13 22:30:16 -0300

    fix app-gradio

--- gradio-json/app-gradio.py
+++ gradio-json/app-gradio.py
@@ -5,7 +5,7 @@ import json
 def envia(json_text):
     objeto_json = json.load(json_text)
 
-demo = gr.Interface(fn=envia, inputs="text", outuputs="text")
+demo = gr.Interface(fn=envia, inputs="text", outputs="text")
 
 if __name__ == "__main__":
     demo.launch(server_name="0.0.0.0", server_port=7860, show_api=False)
